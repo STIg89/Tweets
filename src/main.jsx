19dfd983ab4fb666d9283763aa4c6d8b36dfd622
STIg89 <andreystig89@gmail.com>
2023-06-25 19:38:34 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="Tweets">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter basename="/Tweet/">
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
